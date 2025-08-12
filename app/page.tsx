5410e9bbf4e74699194f738a27d6bd35b90d796c
SRinivas26k <srinivasvarma764@gmail.com>
2025-08-12 18:33:08 +0530

    Add new favicon images for the application
    - Introduced a 16x16 pixel favicon image to enhance branding.
    - Added a 32x32 pixel favicon image for improved visibility on high-resolution displays.
    - Changed the File structre according to the industry standard and agile method for future enhancements.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React from "react";
-import InvoiceSummary from "../components/InvoiceSummary";
-import LineItemsTable from "../components/LineItemsTable";
-import ActionButtons from "../components/ActionButtons";
-import BusinessInfoForm from "@/components/BusinessInfoForm";
-import ClientInfoForm from "@/components/ClientInfoForm";
+import InvoiceSummary from "../components/forms/InvoiceSummary";
+import LineItemsTable from "../components/forms/LineItemsTable";
+import ActionButtons from "../components/invoice/Preview/ActionButtons";
+import BusinessInfoForm from "@/components/forms/BusinessInfoForm";
+import ClientInfoForm from "@/components/forms/ClientInfoForm";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
 const page = () => {
   const InvoicePreview = dynamic(
-    () => import("@/components/InvoicePreview").then((mod) => mod.default),
+    () => import("@/components/invoice/Preview/InvoicePreview").then((mod) => mod.default),
     { ssr: false }
   );
   return (
@@ -26,8 +26,8 @@ const page = () => {
         />
         <h1 className="text-3xl font-bold mb-6">BillForge</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <section>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 ">
+        <section className="space-y-6 space-x-4">
           <BusinessInfoForm />
           <ClientInfoForm />
           <LineItemsTable />
